426390b7100bff8e3370075856e6d5adbab06e94
CarlosHerrera17061048 <carlosherrera99.cih@gmail.com>
2021-04-13 19:28:58 -0600

    Firebase Login con correo

--- PsyProx/app/src/main/java/com/charles/psyprox/LoginChoice.kt
+++ PsyProx/app/src/main/java/com/charles/psyprox/LoginChoice.kt
@@ -2,10 +2,32 @@ package com.charles.psyprox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login_choice.*
 
+enum class  ProviderType{
+    BASIC
+}
 class LoginChoice : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_choice)
+
+        //Setup
+        val bundle:Bundle? = intent.extras
+        val email:String?=bundle?.getString("email")
+        val provider:String?=bundle?.getString("provider")
+        setup(email ?:"",provider ?:"")
+    }
+    private fun setup(email: String, provider: String){
+        txtEmail.text= email
+        txtContra.text= provider
+
+        btnCerrarSesion.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+
     }
 }
--- PsyProx/app/src/main/java/com/charles/psyprox/MainActivity.kt
+++ PsyProx/app/src/main/java/com/charles/psyprox/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.charles.psyprox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.charles.psyprox.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +20,53 @@ class MainActivity : AppCompatActivity() {
         setup()
     }
     private fun setup(){
-      
+      btnRegistro.setOnClickListener {
+          if (edtTextCorreo.text.isNotEmpty() && edtTextPass.text.isNotEmpty()){
+              FirebaseAuth.getInstance()
+                  .createUserWithEmailAndPassword(edtTextCorreo.text.toString(),
+              edtTextPass.text.toString()).addOnCompleteListener {
+                  if(it.isSuccessful){
+                      showLoginChoice(it.result?.user?.email?:"",ProviderType.BASIC)
+                  }else{
+                    showAlert()
+                  }
+              }
+          }
+
+      }
+            btnIniciarSesion.setOnClickListener  {
+                if (edtTextCorreo.text.isNotEmpty() && edtTextPass.text.isNotEmpty()){
+
+                    FirebaseAuth.getInstance()
+                        .signInWithEmailAndPassword(edtTextCorreo.text.toString(),
+                        edtTextPass.text.toString()).addOnCompleteListener {
+                        if(it.isSuccessful){
+                            showLoginChoice(it.result?.user?.email?:"",ProviderType.BASIC)
+                        }else{
+                            showAlert()
+                        }
+                    }
+                }
+
+            }
+    }
+
+    private fun showAlert(){
+
+        val builder = AlertDialog.Builder(this )
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptrar",null )
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+    private fun showLoginChoice(email:String, provider:ProviderType){
+        val LoginChoiceIntent = Intent(this , LoginChoice::class.java).apply {
+        putExtra("email",email)
+            putExtra("provider",provider.name  )
+        }
+        startActivity(LoginChoiceIntent)
+
 
     }
 }
